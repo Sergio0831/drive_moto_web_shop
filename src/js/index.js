f67dd943548eb92b64ce6972856fdb95e73bdfbd
Sergejs Ivcenko <sergejs.ivcenko@gmail.com>
2020-11-01 12:46:00 +0000

    Banner Section
    Banner Slider and Sale Card

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import $ from 'jquery';
-window.$ = window.jQuery = $;
 import 'slick-carousel';
 import 'normalize.css';
 import "../scss/style.scss";
 
 
+// Banner Slider
+$(function() {
+  $(".banner-section__slider").slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"></button>',
+  })
+})
